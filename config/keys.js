9b4681fa0d4e926dc9524366eab611ac270c5809
FarazShaik12 <shaikabdulfaraz@gmail.com>
2021-08-26 14:30:56 +0530

    done

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb://admin:admin@cluster0-shard-00-00.qglk4.mongodb.net:27017,cluster0-shard-00-01.qglk4.mongodb.net:27017,cluster0-shard-00-02.qglk4.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-ud0m33-shard-0&authSource=admin&retryWrites=true&w=majority",
+  secretOrKey: "secret",
+};
